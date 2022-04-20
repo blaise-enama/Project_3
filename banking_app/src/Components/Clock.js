51ce246b3ccea522a4e9e5eb2831165166eb80ca
antonysimos <aseemos8@gmail.com>
2022-04-19 21:30:09 -0400

    greeting

---
+++ banking_app/src/Components/Clock.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import FormComponent from "./FormComponent"
+
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, </h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+export default Clock;
